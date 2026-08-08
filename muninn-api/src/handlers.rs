10fffab4e2247daa9783e12a1af1be2afad0c1be
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2026-08-08 15:36:23 +0600

    feat(handlers): refactor code structure

--- muninn-api/src/handlers.rs
+++ muninn-api/src/handlers.rs
@@ -384,3 +384,4 @@ pub async fn ready_check(State(state): State<AppState>) -> Result<Json<HealthRes
 # 1788294676
 // commit 15 1788294953908283004
 // commit 39 1788294954259386443
+// commit 87 1788294954990837644
