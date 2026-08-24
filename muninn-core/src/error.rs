a9a3ffdd8329f3d8429f0dd646d6e5149ad5afe2
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2026-08-24 13:24:58 +0600

    feat(error): optimize for production

--- muninn-core/src/error.rs
+++ muninn-core/src/error.rs
@@ -89,3 +89,4 @@ pub type Result<T> = std::result::Result<T, Error>;
 // commit 242 1788294957396389231
 // commit 266 1788294957782960537
 // commit 290 1788294958152860832
+// commit 314 1788294958511823330
