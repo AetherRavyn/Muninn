d111d1aa94077da816816915338b783cd8a34bde
swadhinbiswas <swadhinbiswas.cse@gmail.com>
2026-08-28 14:08:02 +0600

    feat(openai): add unit tests

--- muninn-embedding/src/openai.rs
+++ muninn-embedding/src/openai.rs
@@ -141,3 +141,4 @@ impl EmbeddingProvider for OpenAiEmbeddingProvider {
 // commit 136 1788294955753418943
 // commit 160 1788294956122574261
 // commit 328 1788294958736969839
+// commit 352 1788294959121711930
